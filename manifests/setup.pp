098b7efdca9de067c9ff9da85c806d7d4f509ecb
Caius Durling <dev@caius.name>
2016-04-21 23:06:11 +0100

    Setup installs things in correct order

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,16 +1,22 @@
 # Class: jail::setup
 #
-# Lay down the glpbal configuration for jail.conf as well as create the needed
+# Lay down the global configuration for jail.conf as well as create the needed
 # directories and/or zfs mountpoints.
 #
 class jail::setup () {
 
-  package { 'iocage': ensure => installed; }
+  package { 'iocage':
+    ensure => installed;
+  }
+
   service { 'iocage':
-    enable => true,
+    enable => true;
   }
 
   file { '/etc/jail.conf':
-    ensure => absent,
+    ensure => absent;
   }
+
+  File['/etc/jail.conf'] ~> Service['iocage'];
+  Package['iocage'] ~> Service['iocage'];
 }
