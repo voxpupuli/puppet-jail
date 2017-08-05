b3e194067de75a448d64a16a67471bf2ed59120e
Zach Leslie <xaque208@gmail.com>
2017-08-04 21:15:35 -0700

    Update package name
    The name of the iocage package has changed recently to reflect the rewrite in
    python.  Furthermore, it is unclear if the prefix of py36 will remain, as is
    hardcoded here.  As such, we allow tuning the package name.

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -3,9 +3,12 @@
 # Lay down the global configuration for jail.conf as well as create the needed
 # directories and/or zfs mountpoints.
 #
-class jail::setup () {
+class jail::setup (
+  $package_name = 'py36-iocage'
+) {
 
   package { 'iocage':
+    name   => $package_name,
     ensure => installed,
   }
 
